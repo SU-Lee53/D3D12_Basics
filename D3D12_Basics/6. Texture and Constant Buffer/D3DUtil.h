9cffb297dc6001a3f6fdd80e67a810a6ea3277b2
SU-Lee53 <dltmddnr05@naver.com>
2024-12-19 14:51:11 +0900

    5, 6 강의내용 정리

---
+++ D3D12_Basics/6. Texture and Constant Buffer/D3DUtil.h
@@ -0,0 +1,10 @@
+#pragma once
+
+
+
+struct D3DUtils
+{
+	static void SetDebugLayerInfo(ComPtr<ID3D12Device> pD3DDevice);
+
+};
+
---
+++ D3D12_Basics/6. Texture and Constant Buffer/Typedef.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <DirectXMath.h>
+using namespace DirectX;
+
+struct BasicVertex
+{
+	XMFLOAT3 position;
+	XMFLOAT4 color;
+};
