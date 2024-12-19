f2c23370adb49b7b9e659d4b6452431c4b834222
SU-Lee53 <dltmddnr05@naver.com>
2024-12-19 11:45:29 +0900

    03. 사각형 그리기 - IndexBuffer

---
+++ D3D12_Basics/4. DrawIndexedTriangle/BasicMeshObject.h
@@ -0,0 +1,39 @@
+#pragma once
+
+class D3D12Renderer;
+
+class BasicMeshObject
+{
+public:
+	BasicMeshObject();
+	~BasicMeshObject();
+
+	BOOL Initialize(std::shared_ptr<D3D12Renderer> pRenderer);
+	void Draw(ComPtr<ID3D12GraphicsCommandList> pCommandList);
+	BOOL CreateMesh();
+
+private:
+	BOOL InitCommonResources();
+
+	BOOL InitRootSignature();
+	BOOL InitPipelineState();
+
+
+private:
+	// 모든 BasicMeshObject 아래 3가지를 객체들이 공유함
+	static ComPtr<ID3D12RootSignature>	m_pRootSignature;
+	static ComPtr<ID3D12PipelineState>	m_pPipelineState;
+	static DWORD						m_dwInitRefCount;
+
+	std::shared_ptr<D3D12Renderer> m_pRenderer = nullptr;
+
+	ComPtr<ID3D12Resource> m_pVertexBuffer = nullptr;
+	D3D12_VERTEX_BUFFER_VIEW m_VertexBufferView = {};
+	
+	ComPtr<ID3D12Resource> m_pIndexBuffer = nullptr;
+	D3D12_INDEX_BUFFER_VIEW m_IndexBufferView = {};
+
+
+
+};
+
