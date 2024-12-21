a74f22e37eebd56dce7d0e2e80e594b9042f0b71
SU-Lee53 <dltmddnr05@naver.com>
2024-12-21 22:40:25 +0900

    9, 10 강의 내용 정리 + 9 프로젝트 생성

---
+++ D3D12_Basics/9. DepthStencilBuffer/D3DUtil.h
@@ -0,0 +1,16 @@
+#pragma once
+
+
+
+struct D3DUtils
+{
+	static void SetDebugLayerInfo(ComPtr<ID3D12Device> pD3DDevice);
+	static void SetDefaultSamplerDesc(D3D12_STATIC_SAMPLER_DESC* pOutSamplerDesc, UINT RegisterIndex);
+	static inline size_t AlignConstantBuffersize(size_t size)
+	{
+		size_t alligned_size = (size + 255) & (~255);
+		return alligned_size;
+	}
+
+};
+
