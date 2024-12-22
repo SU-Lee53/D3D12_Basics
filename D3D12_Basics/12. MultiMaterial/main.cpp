66aef455da3c0f4776d55ef09535a46910fc0f5f
SU-Lee53 <dltmddnr05@naver.com>
2024-12-22 23:36:04 +0900

    12 프로젝트 생성

--- D3D12_Basics/12. MultiMaterial/main.cpp
+++ D3D12_Basics/12. MultiMaterial/main.cpp
@@ -104,7 +104,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
     // Initialize global strings
     LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-    LoadStringW(hInstance, IDC_MY11OVERLAPPEDFRAME, szWindowClass, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_MY12MULTIMATERIAL, szWindowClass, MAX_LOADSTRING);
     MyRegisterClass(hInstance);
 
     // Perform application initialization:
@@ -114,7 +114,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
         return FALSE;
     }
 
-    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_MY11OVERLAPPEDFRAME));
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_MY12MULTIMATERIAL));
 
     MSG msg;
 
@@ -275,10 +275,10 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
     wcex.cbClsExtra = 0;
     wcex.cbWndExtra = 0;
     wcex.hInstance = hInstance;
-    wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDC_MY11OVERLAPPEDFRAME));
+    wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDC_MY12MULTIMATERIAL));
     wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
     wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-    wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_MY11OVERLAPPEDFRAME);
+    wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_MY12MULTIMATERIAL);
     wcex.lpszClassName = szWindowClass;
     wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
 
