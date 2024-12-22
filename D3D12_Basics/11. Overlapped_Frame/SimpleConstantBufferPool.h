db27d6be9417c3818a3c1cac0c72faecc049d871
SU-Lee53 <dltmddnr05@naver.com>
2024-12-22 16:20:36 +0900

    11. 여러 프레임 중첩 렌더링 프로젝트 생성

---
+++ D3D12_Basics/11. Overlapped_Frame/SimpleConstantBufferPool.h
@@ -0,0 +1,35 @@
+#pragma once
+
+// Constant Buffer 를 다루기 위한 정보들의 구조체
+struct CB_CONTAINER
+{
+	D3D12_CPU_DESCRIPTOR_HANDLE CBVHandle;
+	D3D12_GPU_VIRTUAL_ADDRESS	pGPUMemAddr;
+	UINT8*		pSysMemAddr;
+};
+
+class SimpleConstantBufferPool
+{
+public:
+	SimpleConstantBufferPool();
+	~SimpleConstantBufferPool();
+
+	BOOL Initialize(ComPtr<ID3D12Device5>& pD3DDevice, UINT SizePerCBV, UINT MaxCBVNum);
+
+	CB_CONTAINER* Alloc();
+	void Reset();
+
+private:
+	std::vector<CB_CONTAINER> m_pCBContainerList = {};
+
+	ComPtr<ID3D12DescriptorHeap>	m_pCBVHeap = nullptr;
+	ComPtr<ID3D12Resource>			m_pResource = nullptr;
+	UINT8*	m_pSystemMemAddr = nullptr;
+	UINT	m_SizePerCBV = 0;
+	UINT	m_MaxCBVNum = 0;
+	UINT	m_AllocatedCBVNum = 0;
+
+
+
+};
+
